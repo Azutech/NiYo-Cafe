fe63f2a62df21ff1bceccac2567846680aa6fa71
Azutech <azunna.onugha@gmail.com>
2024-05-17 02:39:45 +0100

    feat () : Email Validations created for users

---
+++ src/users/dto/create-user.dto.ts
@@ -0,0 +1,28 @@
+import { IsNotEmpty, IsEmail, IsString, IsEnum, IsDate } from 'class-validator';
+import { IsEmailDomain } from 'src/utils/emailValidators';
+
+
+enum Gender {
+  Male = 'Male',
+  Female = 'Female',
+}
+
+export class CreateUserDto {
+  @IsNotEmpty()
+  @IsString()
+  fullName: string;
+
+  @IsNotEmpty()
+  @IsEmail()
+  @IsEmailDomain({ message : 'Email domain is not allowed. Allowed domains are: .com, .co.uk, .ng, .org, co.za'})
+  email: string;
+
+  @IsNotEmpty()
+  @IsString()
+  @IsEnum(Gender)
+  gender: Gender;
+
+  @IsNotEmpty()
+  @IsDate()
+  dateOfBirth: Date;
+}
---
+++ src/utils/emailValidators.ts
@@ -0,0 +1,31 @@
+import { registerDecorator, ValidationOptions, ValidatorConstraint, ValidatorConstraintInterface, ValidationArguments } from 'class-validator';
+
+@ValidatorConstraint({ async: false })
+export class IsEmailDomainConstraint implements ValidatorConstraintInterface {
+  validate(email: string, args: ValidationArguments) {
+    // Define the allowed domains
+    const allowedDomains = ['.com', '.co.uk', '.ng', '.org', 'co.za'];
+    
+    // Extract the domain from the email
+    const emailDomain = email.split('@')[1];
+    
+    // Check if the email domain is in the list of allowed domains
+    return allowedDomains.includes(emailDomain);
+  }
+
+  defaultMessage(args: ValidationArguments) {
+    return 'Email domain is not allowed. Allowed domains are: .com, .co.uk, .ng, .org, co.za';
+  }
+}
+
+export function IsEmailDomain(validationOptions?: ValidationOptions) {
+  return function (object: Object, propertyName: string) {
+    registerDecorator({
+      target: object.constructor,
+      propertyName: propertyName,
+      options: validationOptions,
+      constraints: [],
+      validator: IsEmailDomainConstraint,
+    });
+  };
+}
