331833cc39ee2e11ee0727e16498658d572dbc04
Azutech <azunna.onugha@gmail.com>
2024-05-20 03:16:02 +0100

    switch Back

--- src/jwt/jwtStrategy.ts
+++ src/jwt/jwtStrategy.ts
@@ -1,34 +1,18 @@
-import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt, Strategy } from 'passport-jwt';
 import { ConfigService } from '@nestjs/config';
-import { UsersService } from 'src/users/users.service';
-import { User } from 'src/users/model/users';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
-  constructor(
-    private readonly configService: ConfigService,
-    private readonly userService: UsersService,
-  ) {
+  constructor(private readonly configService: ConfigService) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       secretOrKey: configService.get<string>('JWT_SECRET'),
     });
   }
 
-  async validate(payload: any): Promise<User> {
-    try {
-      console.log('JWT payload:', payload);
-      const user = await this.userService.findOne(payload.id);
-      if (!user) {
-        throw new UnauthorizedException('User not found');
-      }
-      return user;
-    } catch (error) {
-      console.error('JWT validation error:', error);
-      throw error;
-    }
+  async validate(payload: any) {
+    return { userId: payload.sub, email: payload.email };
   }
 }
-
